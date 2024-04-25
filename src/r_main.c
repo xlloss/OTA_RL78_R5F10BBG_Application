5f649e45fcb4738a09c0f61e5068af39846cb6f6
Slash.Huang <slash.linux.c@gmail.com>
2024-04-25 20:56:32 +0800

    [main] add update protocol

--- src/r_main.c
+++ src/r_main.c
@@ -33,6 +33,8 @@ Includes
 #include "r_cg_cgc.h"
 #include "r_cg_serial.h"
 /* Start user code for include. Do not edit comment generated here */
+#include "utility.h"
+#include "flash_interface.h"
 /* End user code. Do not edit comment generated here */
 #include "r_cg_userdefine.h"
 
@@ -40,12 +42,32 @@ Includes
 Pragma directive
 ***********************************************************************************************************************/
 /* Start user code for pragma. Do not edit comment generated here */
+/* This section is to hold the version number */
+#pragma section APP_VERSION
+
+//#define OLDER_VER
+
+#ifdef OLDER_VER
+__far const unsigned long version = 0x01010101;
+#else
+__far const unsigned long version = 0x01020305;
+#endif
+//__far const unsigned long version = 0x02020202;
+#pragma section
+
+/* This sections is meant as a placeholder for the CRC, which is placed at the end of ROM */
+#pragma section PROGRAM_CRC
+__far const uint16_t program_crc;
+#pragma section
 /* End user code. Do not edit comment generated here */
 
 /***********************************************************************************************************************
 Global variables and functions
 ***********************************************************************************************************************/
 /* Start user code for global. Do not edit comment generated here */
+uint8_t uart_rcv_buffer[UART_PACKET_LENGTH];
+uint8_t uart_response[UART_RESPONSE_LENGTH];
+extern volatile uint8_t uart_rcv_done;
 /* End user code. Do not edit comment generated here */
 void R_MAIN_UserInit(void);
 
@@ -55,16 +77,52 @@ void R_MAIN_UserInit(void);
 * Arguments    : None
 * Return Value : None
 ***********************************************************************************************************************/
+#pragma section text .BOOT
 void main(void)
 {
+    unsigned char ver_num[10] = "0x01020305";
+    e_packet_handle_t packet_type;
     R_MAIN_UserInit();
     /* Start user code. Do not edit comment generated here */
+
+    R_UART0_Send(ver_num, 10);
+    R_UART0_Send(ver_num, 10);
+
+    /* Prime the UART to receive a packet */
+    R_UART0_Receive(uart_rcv_buffer, UART_PACKET_LENGTH);
+
     while (1U)
     {
-        ;
+        if(uart_rcv_done)
+        {
+            /* Prime the UART to receive a new packet */
+            R_UART0_Receive(uart_rcv_buffer, UART_PACKET_LENGTH);
+            /* Verify packet CRC */
+            /* Run the CRC on the entire packet, including the crc. It should return a crc of 0 */
+            if(general_crc(uart_rcv_buffer, UART_PACKET_LENGTH, CRC_ZERO_SEED) == 0)
+            {
+                /* Get our packet type */
+                packet_type = get_packet_type(uart_rcv_buffer[0]);
+
+                if(packet_type == PACKET_VERSION)
+                {
+                    /* Get the version */
+                    get_version(uart_response);
+
+                    /* Send the current version */
+                    send_response(COMMAND_SEND_VERSION, uart_response);
+                }
+                if(packet_type == PACKET_SWAP_AND_RESET)
+                {
+                    flash_boot_swap_reset();
+                }
+            }
+        }
     }
     /* End user code. Do not edit comment generated here */
 }
+
+#pragma section
 
 /***********************************************************************************************************************
 * Function Name: R_MAIN_UserInit
@@ -75,6 +133,8 @@ void main(void)
 void R_MAIN_UserInit(void)
 {
     /* Start user code. Do not edit comment generated here */
+    init_fcl();
+    R_UART0_Start();
     EI();
     /* End user code. Do not edit comment generated here */
 }
